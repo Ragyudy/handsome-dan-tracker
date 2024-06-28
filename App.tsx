bd853bce3261cdc502fe2f130a1ea85ce787d8c0
ragyudy <gradyzyu@gmail.com>
2024-06-28 10:17:02 -0400

    stack navigator

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-const HelloWorldApp = () => {
+type RootStackParamList = {Home: undefined; BlankScreen: undefined};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const Home: React.FC<{navigation: any}> = ({navigation}) => {
   return (
     <View
       style={{
@@ -10,8 +18,39 @@ const HelloWorldApp = () => {
         alignItems: 'center',
         backgroundColor: 'lightblue',
       }}>
-      <Text>Handsome Dan Tracker</Text>
+      <Button
+        title="Handsome Dan Tracker"
+        onPress={() => navigation.navigate('BlankScreen')}
+      />
     </View>
   );
 };
-export default HelloWorldApp;
+
+const BlankScreen: React.FC = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgreen',
+      }}>
+      <Text>Blank Screen</Text>
+    </View>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="BlankScreen" component={BlankScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
